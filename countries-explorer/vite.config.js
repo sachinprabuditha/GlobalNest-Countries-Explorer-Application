7506ae48f106228d16c43c6df178d1a16f0f9cc0
Sachin Prabuditha <125368889+sachinprabuditha@users.noreply.github.com>
2025-05-02 16:43:54 +0530

    feat: add path alias for leaflet in Vite configuration

--- countries-explorer/vite.config.js
+++ countries-explorer/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'leaflet': path.resolve(__dirname, 'node_modules/leaflet'),
+    },
+  },
   optimizeDeps: {
     include: ['leaflet'],
   },
-})
+});
